b54335845b64e1caabba757049507a5c9d8c36ad
Hamm <majhamm@qq.com>
2024-05-24 01:08:13 +0800

    fix(BUG): 修复了一些已知问题
    Signed-off-by: Hamm <majhamm@qq.com>

--- src/main/java/cn/hamm/demo/module/system/IndexController.java
+++ src/main/java/cn/hamm/demo/module/system/IndexController.java
@@ -5,7 +5,8 @@ import cn.hamm.airpower.annotation.Description;
 import cn.hamm.airpower.annotation.DesensitizeExclude;
 import cn.hamm.airpower.model.Json;
 import cn.hamm.airpower.root.RootController;
-import cn.hamm.demo.module.TestEntity;
+import cn.hamm.airpower.task.TaskFlow;
+import cn.hamm.demo.module.user.UserEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -16,10 +17,30 @@ import org.springframework.web.bind.annotation.GetMapping;
 @ApiController("/")
 @Description("首页")
 public class IndexController extends RootController {
-
     @GetMapping("")
     @DesensitizeExclude
     public Json index() {
-        return Json.data(new TestEntity());
+        TaskFlow.init(new UserEntity().setNickname("Hamm").setId(1L).setEmail("admin@hamm.cn"))
+                .next(user -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return TaskFlow.next(user.setNickname("Hamm2"));
+                }).next(user -> {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(user.getNickname());
+                    return TaskFlow.next(user.setNickname("Hamm3"));
+                })
+                .onSuccess(user -> {
+                    System.out.println("Success " + user.getNickname());
+                })
+                .start();
+        return Json.success("Hello World");
     }
 }
