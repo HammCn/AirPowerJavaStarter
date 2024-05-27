8604e857eb034ee6217ff050408f84ed94893938
Hamm <majhamm@qq.com>
2024-05-28 00:17:32 +0800

    fix(BUG): 修复了一些已知问题
    Signed-off-by: Hamm <majhamm@qq.com>

--- src/main/java/cn/hamm/demo/common/AppWebSocketHandler.java
+++ src/main/java/cn/hamm/demo/common/AppWebSocketHandler.java
@@ -6,6 +6,7 @@ import cn.hamm.airpower.websocket.WebSocketPayload;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
 
 /**
  * <h1>应用自定义的事件处理器</h1>
@@ -16,8 +17,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppWebSocketHandler extends WebSocketHandler {
     @Override
-    public void onWebSocketPayload(@NotNull WebSocketPayload webSocketPayload) {
-        super.onWebSocketPayload(webSocketPayload);
+    public void onWebSocketPayload(@NotNull WebSocketPayload webSocketPayload, @NotNull WebSocketSession session) {
+        super.onWebSocketPayload(webSocketPayload, session);
         Utils.getWebsocketUtil().publish(webSocketPayload);
     }
 }
