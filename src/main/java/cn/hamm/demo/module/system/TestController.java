9117451f6a5dcf1695a75f454ca52612f8a4eac8
Hamm <majhamm@qq.com>
2024-08-14 11:08:33 +0800

    fix(Request): 调整防重放时长和获取客户端IP的方式

--- src/main/java/cn/hamm/demo/module/system/TestController.java
+++ src/main/java/cn/hamm/demo/module/system/TestController.java
@@ -5,6 +5,7 @@ import cn.hamm.airpower.annotation.Description;
 import cn.hamm.airpower.annotation.Permission;
 import cn.hamm.airpower.model.Json;
 import cn.hamm.airpower.root.RootController;
+import cn.hamm.airpower.util.Utils;
 import org.springframework.web.bind.annotation.GetMapping;
 
 /**
@@ -19,5 +20,10 @@ public class TestController extends RootController {
     @GetMapping("")
     public Json index() {
         return Json.success("Hello World!");
+    }
+
+    @GetMapping("ip")
+    public Json ip() {
+        return Json.success(Utils.getRequestUtil().getIpAddress(Utils.getRequest()));
     }
 }
