4ec514f91d6c4de744291cc8b97432c0597bae0a
Hamm <majhamm@qq.com>
2024-09-06 16:34:41 +0800

    禁用权限控制器的默认授权在PermissionController中，默认授权已被禁用，以允许根据自定义条件进行权限检查。这是通过在getList方法上添加@Permission注解并设置authorize为false实现的。

--- src/main/java/cn/hamm/demo/module/system/permission/PermissionController.java
+++ src/main/java/cn/hamm/demo/module/system/permission/PermissionController.java
@@ -2,6 +2,7 @@ package cn.hamm.demo.module.system.permission;
 
 import cn.hamm.airpower.annotation.ApiController;
 import cn.hamm.airpower.annotation.Description;
+import cn.hamm.airpower.annotation.Permission;
 import cn.hamm.airpower.model.Json;
 import cn.hamm.airpower.model.query.QueryRequest;
 import cn.hamm.airpower.util.Utils;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 @ApiController("permission")
 @Description("权限")
 public class PermissionController extends BaseController<PermissionEntity, PermissionService, PermissionRepository> {
+    @Permission(authorize = false)
     @Override
     public Json getList(@RequestBody QueryRequest<PermissionEntity> queryRequest) {
         return Json.data(Utils.getTreeUtil().buildTreeList(service.getList(queryRequest)));
