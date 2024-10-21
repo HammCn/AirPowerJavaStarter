2c9275c6e541f45e3b4f3bdd32111d06d484a223
Hamm <majhamm@qq.com>
2024-10-21 19:19:35 +0800

    fix(Config): 移除了获取拦截器的方法,改为直接添加拦截器

--- src/main/java/cn/hamm/demo/WebConfig.java
+++ src/main/java/cn/hamm/demo/WebConfig.java
@@ -1,12 +1,13 @@
 package cn.hamm.demo;
 
 import cn.hamm.airpower.AbstractWebConfig;
-import cn.hamm.airpower.interceptor.AbstractRequestInterceptor;
 import cn.hamm.airpower.websocket.WebSocketHandler;
 import cn.hamm.demo.common.AppWebSocketHandler;
 import cn.hamm.demo.common.interceptor.RequestInterceptor;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 /**
  * <h1>全局配置</h1>
@@ -27,7 +28,8 @@ public class WebConfig extends AbstractWebConfig {
     }
 
     @Override
-    public AbstractRequestInterceptor getAccessInterceptor() {
-        return requestInterceptor;
+    public void addInterceptors(@NotNull InterceptorRegistry registry) {
+        registry.addInterceptor(requestInterceptor)
+                .addPathPatterns("/**");
     }
 }
